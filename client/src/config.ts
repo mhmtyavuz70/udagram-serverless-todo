2a9ef6e9b7a5e35f318e3068dd3638747570ee32
mhmtyavuz70 <66701167+mhmtyavuz70@users.noreply.github.com>
2020-07-13 11:00:17 +0300

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ud06arlxik';
+const apiId = 'csv29g1jm7';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
 export const authConfig = {
-  domain: 'myavuz.auth0.com',
+  domain: 'myavuz.us.auth0.com',
   clientId: 'XTiqYowk9e0zXhPzpTzJD3kFrZ2MAFfE',
   callbackUrl: 'http://localhost:3000/callback'
 };
